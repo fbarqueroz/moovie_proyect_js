81ef3e061f98506840c7f4a15bc308d8ee5f7372
fbarqueroz <fabianbarquero9@gmail.com>
2020-11-15 23:05:10 -0600

    JS changes

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@
 function addMovies(movies) {
   const movieList = document.getElementById ('movie_list');
   movie_list.innerHTML = '';
-  windows.scroll (0,0);
 
   for (let i = 0; movies.length; i += 1) {
     let listItem = document.createElement ('li');
     movieList.appendChild(listItem);
+    window.scroll(0,0);
+
+    //Poster
+    
 
   // Movie title
     let title = document.createElement ('h2');
@@ -36,17 +39,37 @@ function addMovies(movies) {
     metascore.innerHTML = movies[i].Metascore;
     listItem.appendChild (metascore);
   }
-}
+};
 
 const movie_form = document.getElementById ('form');
 movie_form.addEventListener ('submit',(event) => {
-  event.preventDefault;
-  const movie_search = movie_form.element[0].value;
+  event.preventDefault();
+  const movie_search = movie_form.elements[0].value;
+  console.log (movie_search);
 
-   // Fetch
+    // Fetch
   fetch(`http://www.omdbapi.com/?apikey=4f567dc7&s=${movie_search}`)
     .then((response) => response.json())
     .then((data) => {
       addMovies (data.Search);
     });
 });
+
+// Load More buttom
+
+const nextPage = '';
+let count = 1;
+const loadMore = document.getElementById ('load_more');
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=4f567dc7 --> 
+
+loadMore.addEventListener ('click', (event) => {
+  event.preventDefault ();
+  const save_search = search.element[0].value;
+  fetch(`http://www.omdbapi.com/?apikey=4f567dc7&s=${count}`)
+    .then((response) => response.json())
+    .then((data) => {
+      addMovies (data.Search);
+    });
+})
+
