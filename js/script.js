899e63919e40160a4ba391fa387fdfa9be7c271b
fbarqueroz <fabianbarquero9@gmail.com>
2020-11-17 00:03:57 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@
 //
 
 function addMovies(movies) {
-  const movieList = document.getElementById ('movie_list');
-  movie_list.innerHTML = '';
-  window.scroll (0,0);
-
-  //const tooltip = document.getElementById ('tooltip') 
+  const movieList = document.getElementById('movie_list');
+  movieList.innerHTML = '';
+  window.scroll(0, 0);
 
   for (let i = 0; movies.length; i += 1) {
-    let listItem = document.createElement ('li');
+    const listItem = document.createElement('li');
     movieList.appendChild(listItem);
 
     // Fetch
@@ -31,77 +29,41 @@ function addMovies(movies) {
           `;
         listItem.innerHTML = movie_description;
     });
-
-    /*//Poster
-    let poster = document.createElement ('img');
-    poster.setAttribute ('src',movies[i].Poster);
-    listItem.appendChild (poster);
-
-    // Movie title
-    let title = document.createElement ('h2');
-    title.innerHTML = movies[i].Title;
-    listItem.appendChild (title);
-  
-    // Movie year
-    let movieYear = document.createElement ('h3');
-    movieYear.innerHTML = movies[i].Year;
-    listItem.appendChild (movieYear);
-    
-    // Runtime
-    const runtime = document.createElement ('h4');
-    runtime.innerHTML = movies[i].Runtime;
-    listItem.appendChild (runtime);
-
-    // Movie Actors
-    const actors = document.createElement ('p');
-    actors.innerHTML = movies[i].Actors;
-    listItem.appendChild (actors);
-
-    //Metascore
-    const metascore = document.createElement ('p');
-    metascore.innerHTML = movies[i].Metascore;
-    listItem.appendChild (metascore);*/
-
-    
   }
-};
-
-const movie_form = document.getElementById ('form');
-movie_form.addEventListener ('submit',(event) => {
+}
+const movie_form = document.getElementById('form');
+movie_form.addEventListener('submit', (event) => {
   event.preventDefault();
   const movie_search = movie_form.elements[0].value;
 
-    // Fetch
+  // Fetch
   fetch(`http://www.omdbapi.com/?apikey=4f567dc7&s=${movie_search}`)
     .then((response) => response.json())
     .then((data) => {
-      addMovies (data.Search);
+      addMovies(data.Search);
     });
 });
 
 // Load More buttom
-
-const nextPage = '';
 let count = 1;
-const loadMore = document.getElementById ('load_more');
-// http://www.omdbapi.com/?i=tt3896198&apikey=4f567dc7 --> original link (guardians of galaxy Vol.2) 
-loadMore.addEventListener ('click', (event) => {
-  event.preventDefault ();
+const loadMore = document.getElementById('load_more');
+// http://www.omdbapi.com/?i=tt3896198&apikey=4f567dc7 --> original link (guardians of galaxy Vol.2)
+loadMore.addEventListener('click', (event) => {
+  event.preventDefault();
   const searching = search.element[0].value;
-  count ++;
+  count = count++;
   fetch(`http://www.omdbapi.com/?apikey=4f567dc7&s=${searching}&page=${count}`)
     .then((response) => response.json())
     .then((data) => {
-      addMovies (data.Search);
+      addMovies(data.Search);
     });
 
-    fetch(`http://www.omdbapi.com/?apikey=4f567dc7&i=${searching}&page=${count}`)
+  fetch(`http://www.omdbapi.com/?apikey=4f567dc7&i=${searching}&page=${count}`)
   .then((response) => response.json())
   .then((data) => {
-    addMovies (data.Search);
-    addMovies (data.Runtime);
-    addMovies (data.Actors);
-    addMovies (data.Metascore);
+    addMovies(data.Search);
+    addMovies(data.Runtime);
+    addMovies(data.Actors);
+    addMovies(data.Metascore);
   });
 });
-
