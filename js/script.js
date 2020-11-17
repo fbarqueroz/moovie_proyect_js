68d89d3a5787b3b978b63e6c25b7e7a4b1c054cc
fbarqueroz <fabianbarquero9@gmail.com>
2020-11-16 20:28:42 -0600

    changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 function addMovies(movies) {
   const movieList = document.getElementById ('movie_list');
   movie_list.innerHTML = '';
+  window.scroll (0,0);
 
   //const tooltip = document.getElementById ('tooltip') 
 
@@ -12,23 +13,57 @@ function addMovies(movies) {
     let listItem = document.createElement ('li');
     movieList.appendChild(listItem);
 
-    //Tooltip
+    // Fetch
+    fetch(`http://www.omdbapi.com/?apikey=4f567dc7&i=${movies[i].imdbID}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const movie_description = `
+        <div class="movie_description">
+          <li><p>${movies[i].Title}</p></li>
+          <li><img src="${movies[i].Poster}" alt="movies.Title" class="clearfix"></li>
+          <!--Modal-->
+          <div class="modal">
+          <button id="myBtn">More Details</button>
+              <div id="myModal" class="modal">
+                  <div class="modal-content">
+                      <span class="close">&times;</span>
+                      <li><p>Year: ${movies[i].Year}</p></li>
+                      <li><p>Runtime: ${data.Runtime}</p></li>
+                      <li><p>Actors: ${data.Actors}</p></li>
+                      <li><p>Metascore: ${data.Metascore}</p></li>
+                  </div>
+              </div>
+          </div>
+        </div>
+          `;
+        listItem.innerHTML = movie_description;
 
-    const movie_description = `
-    <div class="movie_description">
-      <img src="${movies[i].Poster}" alt="movies.Title" class="clearfix">
-      <span>Title: ${movies[i].Title}</span>
-      <br>
-      <span>Year: ${movies[i].Year}</span>
-      <br>
-      <span>Runtime: ${movies[i].Runtime}</span>
-      <br>
-      <span>Actors: ${movies[i].Actors}</span>
-      <br>
-      <span>Metascore: ${movies[i].Metascore}</span>
-    </div>
-    `;
-    listItem.innerHTML = movie_description;
+        // Get the modal
+        let modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        let btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        let span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+          }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        };
+    });
 
     /*//Poster
     let poster = document.createElement ('img');
@@ -59,6 +94,8 @@ function addMovies(movies) {
     const metascore = document.createElement ('p');
     metascore.innerHTML = movies[i].Metascore;
     listItem.appendChild (metascore);*/
+
+    
   }
 };
 
@@ -80,25 +117,24 @@ movie_form.addEventListener ('submit',(event) => {
 const nextPage = '';
 let count = 1;
 const loadMore = document.getElementById ('load_more');
-
 // http://www.omdbapi.com/?i=tt3896198&apikey=4f567dc7 --> original link (guardians of galaxy Vol.2) 
-
 loadMore.addEventListener ('click', (event) => {
   event.preventDefault ();
   const searching = search.element[0].value;
+  count ++;
   fetch(`http://www.omdbapi.com/?apikey=4f567dc7&s=${searching}&page=${count}`)
     .then((response) => response.json())
     .then((data) => {
       addMovies (data.Search);
-      count++;
     });
+
+    fetch(`http://www.omdbapi.com/?apikey=4f567dc7&i=${searching}&page=${count}`)
+  .then((response) => response.json())
+  .then((data) => {
+    addMovies (data.Search);
+    addMovies (data.Runtime);
+    addMovies (data.Actors);
+    addMovies (data.Metascore);
+  });
 });
 
-fetch(`http://www.omdbapi.com/?apikey=4f567dc7&i=${searching}&page=${count}`)
-.then((response) => response.json())
-.then((data) => {
-  addMovies (data.Search);
-  addMovies (data.Runtime);
-  addMovies (data.Actors);
-  addMovies (data.Metascore);
-});
